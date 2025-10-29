b598f4b9db8d294d11bfd13bb9880a9812be73d2
Ethan Lee <pluscompanyhqDEPT@gmail.com>
2025-10-29 20:07:28 +0800

    Modify calculations example

--- examples/calculations.js
+++ examples/calculations.js
@@ -1,16 +1,16 @@
-procedure add(val1, val2) {
+function add(val1, val2) {
     return val1 + val2;
 }
 
-procedure subtract(val1, val2) {
+function subtract(val1, val2) {
     return val1 - val2;
 }
 
-procedure multiply(val1, val2) {
+function multiply(val1, val2) {
     return val1 * val2;
 }
 
-procedure divide(val1, val2) {
+function divide(val1, val2) {
     return val1 / val2;
 }
 
